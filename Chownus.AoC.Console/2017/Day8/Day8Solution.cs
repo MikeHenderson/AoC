e4f873388ea5db925d166a6d85eeab19b899f6f5
Mike Henderson <henderson.mike.j@gmail.com>
2017-12-07 23:48:54 -0600

    Implemented day 8

---
+++ Chownus.AoC.Console/2017/Day8/Day8Solution.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chownus.AoC.Console
+{
+    public class Day8Solution : IAoCSolution
+    {
+        public int Day => 8;
+        private IEnumerable<string> _input;
+
+        private static readonly IDictionary<string, Func<int, int, bool>> _operators = new Dictionary<string, Func<int, int, bool>>
+        {
+            { ">", (x, y) => x > y},
+            { ">=", (x, y) => x >= y},
+            { "<", (x, y) => x < y},
+            { "<=", (x, y) => x <= y},
+            { "!=", (x, y) => x != y},
+            { "==", (x, y) => x == y},
+        };
+
+        private static readonly IDictionary<string, Func<int, int, int>> _modifiers = new Dictionary<string, Func<int, int, int>>
+        {
+            { "inc", (x, y) => x + y },
+            { "dec", (x, y) => x - y }
+        };
+
+
+        public string RunPart1()
+        {
+            var registers = new Dictionary<string, int>();
+
+            foreach (var instruction in _input)
+                 ExecuteInstruction(instruction, registers);
+
+            return registers.Max(x => x.Value).ToString();
+        }
+
+        public string RunPart2()
+        {
+            var registers = new Dictionary<string, int>();
+            int maxValue = 0;
+
+            foreach (var instruction in _input)
+            {
+                var currentMax = ExecuteInstruction(instruction, registers);
+
+                if (currentMax > maxValue)
+                    maxValue = currentMax;
+            }
+
+            return maxValue.ToString();
+        }
+
+        private int ExecuteInstruction(string instruction, IDictionary<string, int> registers)
+        {
+            var parts = instruction.Split(' ');
+
+            var targetRegister = parts[0];
+            var modifier = parts[1];
+            var modifierValue = int.Parse(parts[2]);
+
+            if (!registers.ContainsKey(targetRegister))
+                registers.Add(targetRegister, 0);
+
+            var checkRegister = parts[4];
+            var operand = parts[5];
+            var checkValue = int.Parse(parts[6]);
+
+            if (!registers.ContainsKey(checkRegister))
+                registers.Add(checkRegister, 0);
+
+            bool conditionMet = _operators[operand](registers[checkRegister], checkValue);
+
+            if (conditionMet)
+                registers[targetRegister] = _modifiers[modifier](registers[targetRegister], modifierValue);
+
+            return registers.Max(x => x.Value);
+        }
+
+        public void Initialize(IEnumerable<string> input)
+        {
+            _input = input;
+        }
+    }
+}
---
+++ Chownus.AoC.Tests/2017/Day8/Day8Tests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Chownus.AoC.Console;
+using Xunit;
+
+namespace Chownus.AoC.Tests
+{
+    public class Day8Tests
+    {
+        private readonly IAoCSolution _testObject;
+
+        public Day8Tests()
+        {
+            var input = new List<string>
+            {
+                "b inc 5 if a > 1",
+                "a inc 1 if b < 5",
+                "c dec -10 if a >= 1",
+                "c inc -20 if c == 10",
+            };
+
+            _testObject = new Day8Solution();
+            _testObject.Initialize(input);
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            var solution = _testObject.RunPart1();
+            Assert.Equal("1", solution);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            var solution = _testObject.RunPart2();
+            Assert.Equal("10", solution);
+        }
+    }
+}
