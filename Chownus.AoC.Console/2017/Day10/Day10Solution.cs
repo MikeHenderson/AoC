6ae04eb6a55f0f970df18b28acf787b515e8b23b
Mike Henderson <henderson.mike.j@gmail.com>
2017-12-10 00:44:17 -0600

    Day 10 part 1

---
+++ Chownus.AoC.Console/2017/Day10/Day10Solution.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chownus.AoC.Console
+{
+    public class Day10Solution : IAoCSolution
+    {
+        private IEnumerable<int> _input;
+
+        public int Day => 10;
+
+        public string RunPart1()
+        {
+            var list = GenerateList();
+            var skipCount = 0;
+            var current = list.First;
+
+            foreach (var direction in _input)
+            {
+                current = ReverseSection(current, direction);
+
+                current = Skip(current, skipCount);
+
+                skipCount++;
+            }
+
+            return (list.First.Value * list.First.Next.Value).ToString();
+        }
+
+        private LinkedListNode<int> ReverseSection(LinkedListNode<int> current, int direction)
+        {
+            var subset = new int[direction];
+
+            for (int i = 0; i < direction; i++)
+            {
+                subset[i] = current.Value;
+
+                if(i < direction - 1)
+                    current = GetNextOrFirst(current);
+            }
+
+            for (int i = 0; i < direction; i++)
+            {
+                current.Value = subset[i];
+
+                if(i < direction - 1)
+                    current = GetPreviousOrLast(current);
+            }
+
+            // Skip ahead forward to the next one
+            return Skip(current, direction);
+        }
+
+        public string RunPart2()
+        {
+            return "";
+        }
+
+        public void Initialize(IEnumerable<string> input)
+        {
+            _input = input.First().Split(',').Select(int.Parse);
+        }
+
+        private LinkedList<int> GenerateList()
+        {
+            var list = new LinkedList<int>();
+
+            var head = list.AddFirst(0);
+
+            for (int i = 1; i <= 255; i++)
+                head = list.AddAfter(head, i);
+
+            return list;
+        }
+
+        // Create the behavior of a circularly linked list
+        private LinkedListNode<int> GetNextOrFirst(LinkedListNode<int> current)
+        {
+            return current.Next ?? current.List.First;
+        }
+
+        private LinkedListNode<int> GetPreviousOrLast(LinkedListNode<int> current)
+        {
+            return current.Previous ?? current.List.Last;
+        }
+
+        private LinkedListNode<int> Skip(LinkedListNode<int> current, int count)
+        {
+            if (count == 0) return current;
+
+            while (count > 0)
+            {
+                current = GetNextOrFirst(current);
+                count--;
+            }
+
+            return current;
+        }
+    }
+}
---
+++ Chownus.AoC.Tests/2017/Day10/Day10Tests.cs
@@ -0,0 +1,29 @@
+﻿using Chownus.AoC.Console;
+using Xunit;
+
+namespace Chownus.AoC.Tests
+{
+    public class Day10Tests
+    {
+        private readonly IAoCSolution _testObject;
+
+        public Day10Tests()
+        {
+            _testObject = new Day10Solution();
+            _testObject.Initialize(new [] { "3,4,1,5" });
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            var solution = _testObject.RunPart1();
+            Assert.Equal("12", solution);
+        }
+
+        [Fact] 
+        public void Part2()
+        {
+
+        }
+    }
+}
