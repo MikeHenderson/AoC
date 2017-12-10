a25eb8f608473511e46bbc6d5ce7846330ae5ba9
Mike Henderson <henderson.mike.j@gmail.com>
2017-12-10 02:37:38 -0600

    Day 10 Part 2..... finally

--- Chownus.AoC.Console/2017/Day10/Day10Solution.cs
+++ Chownus.AoC.Console/2017/Day10/Day10Solution.cs
@@ -1,22 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace Chownus.AoC.Console
 {
     public class Day10Solution : IAoCSolution
     {
         private IEnumerable<int> _input;
+        private string _input2;
 
         public int Day => 10;
 
         public string RunPart1()
         {
-            var list = GenerateList();
-            var skipCount = 0;
-            var current = list.First;
+            var count = 0;
+            var knotHash = GenerateSparseHashFromInput(GenerateHashList().First, _input, ref count);
+            return (knotHash.List.First.Value * knotHash.List.First.Next.Value).ToString();
+        }
 
-            foreach (var direction in _input)
+        public string RunPart2()
+        {
+            // Get sparse hash array
+            var list = GenerateHashList();
+            var input = GenerateAsciiInputList();
+            var head = list.First;
+            var skipCount = 0;
+
+            for (int i = 0; i < 64; i++)
+                head = GenerateSparseHashFromInput(head, input, ref skipCount);
+
+            // Generate dense hash from sparse hash
+            var denseHash = new List<int>();
+
+            for(int i = 0; i < 16; i++)
+                denseHash.Add(list.Skip(i * 16).Take(16).Aggregate(0, (x, y) => x ^ y));
+
+            return string.Join(string.Empty, denseHash.Select(x => x.ToString("X2")));
+        }
+
+        public void Initialize(IEnumerable<string> input)
+        {
+            _input = input.First().Split(',').Select(int.Parse);
+            _input2 = input.First().Replace(" ", string.Empty);
+        }
+
+        private LinkedList<int> GenerateHashList()
+        {
+            var list = new LinkedList<int>();
+
+            var head = list.AddFirst(0);
+
+            for (int i = 1; i < 256; i++)
+                head = list.AddAfter(head, i);
+
+            return list;
+        }
+
+        private IList<int> GenerateAsciiInputList()
+        {
+            // Get original ascii codes and add appendix
+            var asciiCodes = _input2.Select(x => (int) x).ToList();
+            asciiCodes.AddRange(new[] {17, 31, 73, 47, 23});
+
+            return asciiCodes;
+        }
+
+        private LinkedListNode<T> GenerateSparseHashFromInput<T>(LinkedListNode<T> current, IEnumerable<int> input, ref int skipCount)
+        {
+            foreach (var direction in input)
             {
                 current = ReverseSection(current, direction);
 
@@ -25,18 +77,18 @@ namespace Chownus.AoC.Console
                 skipCount++;
             }
 
-            return (list.First.Value * list.First.Next.Value).ToString();
+            return current;
         }
 
-        private LinkedListNode<int> ReverseSection(LinkedListNode<int> current, int direction)
+        private LinkedListNode<T> ReverseSection<T>(LinkedListNode<T> current, int direction)
         {
-            var subset = new int[direction];
+            var subset = new T[direction];
 
             for (int i = 0; i < direction; i++)
             {
                 subset[i] = current.Value;
 
-                if(i < direction - 1)
+                if (i < direction - 1)
                     current = GetNextOrFirst(current);
             }
 
@@ -44,7 +96,7 @@ namespace Chownus.AoC.Console
             {
                 current.Value = subset[i];
 
-                if(i < direction - 1)
+                if (i < direction - 1)
                     current = GetPreviousOrLast(current);
             }
 
@@ -52,40 +104,18 @@ namespace Chownus.AoC.Console
             return Skip(current, direction);
         }
 
-        public string RunPart2()
-        {
-            return "";
-        }
-
-        public void Initialize(IEnumerable<string> input)
-        {
-            _input = input.First().Split(',').Select(int.Parse);
-        }
-
-        private LinkedList<int> GenerateList()
-        {
-            var list = new LinkedList<int>();
-
-            var head = list.AddFirst(0);
-
-            for (int i = 1; i <= 255; i++)
-                head = list.AddAfter(head, i);
-
-            return list;
-        }
-
         // Create the behavior of a circularly linked list
-        private LinkedListNode<int> GetNextOrFirst(LinkedListNode<int> current)
+        private LinkedListNode<T> GetNextOrFirst<T>(LinkedListNode<T> current)
         {
             return current.Next ?? current.List.First;
         }
 
-        private LinkedListNode<int> GetPreviousOrLast(LinkedListNode<int> current)
+        private LinkedListNode<T> GetPreviousOrLast<T>(LinkedListNode<T> current)
         {
             return current.Previous ?? current.List.Last;
         }
 
-        private LinkedListNode<int> Skip(LinkedListNode<int> current, int count)
+        private LinkedListNode<T> Skip<T>(LinkedListNode<T> current, int count)
         {
             if (count == 0) return current;
 
--- Chownus.AoC.Tests/2017/Day10/Day10Tests.cs
+++ Chownus.AoC.Tests/2017/Day10/Day10Tests.cs
@@ -10,12 +10,12 @@ namespace Chownus.AoC.Tests
         public Day10Tests()
         {
             _testObject = new Day10Solution();
-            _testObject.Initialize(new [] { "3,4,1,5" });
         }
 
         [Fact]
         public void Part1()
         {
+            _testObject.Initialize(new[] { "3,4,1,5" });
             var solution = _testObject.RunPart1();
             Assert.Equal("12", solution);
         }
@@ -23,7 +23,9 @@ namespace Chownus.AoC.Tests
         [Fact] 
         public void Part2()
         {
-
+            _testObject.Initialize(new[] { "1,2,3" });
+            var solution = _testObject.RunPart2();
+            Assert.Equal("3efbe78a8d82f29979031a4aa0b16a9d", solution.ToLower());
         }
     }
 }
