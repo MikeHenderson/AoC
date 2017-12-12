25b18ce53df3fd68ef8c3f002dd0e29e8c9f942a
Mike Henderson <henderson.mike.j@gmail.com>
2017-12-11 23:37:39 -0600

    Day 12 Part 1..... grotty... very very grotty... I feel dirty doing this

--- Chownus.AoC.Console/2017/12/Day12Solution.cs
+++ Chownus.AoC.Console/2017/12/Day12Solution.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Chownus.AoC.Console
 {
@@ -10,12 +11,61 @@ namespace Chownus.AoC.Console
         public int Day => 12;
         public string RunPart1(IEnumerable<string> testData)
         {
-            return "";
+            var input = new Dictionary<int, IList<int>>();
+            var visited = new List<int>();
+
+            foreach (var line in testData)
+            {
+                var parts = line.Split(new[] {"<->"}, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => x.Trim())
+                    .ToList();
+
+                input.Add(int.Parse(parts[0]), parts[1].Split(',').Select(int.Parse).ToList());
+            }
+
+            var count = 0;
+
+            // LOL this is bad.....
+            for (int i = 0; i < 100; i++)
+            {
+                foreach (var kvp in input)
+                {
+                    var visitedKey = CanTalkWithRoot(kvp, input, visited);
+
+                    if (visitedKey != null)
+                    {
+                        if (!visited.Contains(kvp.Key))
+                            visited.Add(kvp.Key);
+
+                        foreach (var val in kvp.Value)
+                        {
+                            if (!visited.Contains(val))
+                                visited.Add(val);
+                        }
+
+                    }
+                }
+            }
+
+            return visited.Count.ToString();
         }
 
         public string RunPart2(IEnumerable<string> testData)
         {
             return "";
+        }
+
+        private int? CanTalkWithRoot(KeyValuePair<int, IList<int>> kvp, IDictionary<int, IList<int>> lookup, IEnumerable<int> visited)
+        {
+            if (kvp.Key == 0) return kvp.Key;
+
+            if (kvp.Value.Intersect(visited).Any()) return kvp.Key;
+
+            if (lookup[0].Contains(kvp.Key)) return kvp.Key;
+
+            if (lookup[kvp.Key].Contains(0)) return kvp.Key;
+
+            return null;
         }
     }
 }
--- Chownus.AoC.Tests/2017/12/Day12Tests.cs
+++ Chownus.AoC.Tests/2017/12/Day12Tests.cs
@@ -1,4 +1,6 @@
-﻿using Chownus.AoC.Console;
+﻿using System.Collections;
+using System.Collections.Generic;
+using Chownus.AoC.Console;
 using Xunit;
 
 namespace Chownus.AoC.Tests
@@ -6,21 +8,38 @@ namespace Chownus.AoC.Tests
     public class Day12Tests
     {
         private readonly IAoCSolution _testObject;
+        private readonly IList<string> _input;
+
 
         public Day12Tests()
         {
-            _testObject = new Day11Solution();
+            _testObject = new Day12Solution();
+            _input = new List<string>
+            {
+                "0 <-> 2",
+                "1 <-> 1",
+                "2 <-> 0, 3, 4",
+                "3 <-> 2, 4",
+                "4 <-> 2, 3, 6",
+                "5 <-> 6",
+                "6 <-> 4, 5"
+            };
         }
 
         [Fact]
         public void Part1()
         {
+            var solution = _testObject.RunPart1(_input);
 
+            Assert.Equal("6", solution);
         }
 
         [Fact]
         public void Part2()
         {
+            var solution = _testObject.RunPart1(_input);
+
+            Assert.Equal("2", solution);
         }
     }
 }
