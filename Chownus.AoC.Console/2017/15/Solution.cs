ba07a50d2646553d084b363b0c46fb9900fb013b
Mike Henderson <henderson.mike.j@gmail.com>
2017-12-16 00:11:29 -0600

    Day 15 Solutions

---
+++ Chownus.AoC.Console/2017/15/Solution.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace Chownus.AoC.Console._2017._15
+{
+    public class Solution : IAoCSolution
+    {
+        public int Day => 15;
+
+        public string RunPart1(IEnumerable<string> testData)
+        {
+            var parts = testData.First().Split(',');
+            var a = long.Parse(parts[0]);
+            var b = long.Parse(parts[1]);
+
+            var aFactor = 16807L;
+            var bFactor = 48271L;
+            var divider = 2147483647L;
+            var counter = 0;
+
+            for(long i = 0; i < 40000000; i++)
+            {
+                a = (a * aFactor) % divider;
+                b = (b * bFactor) % divider;
+
+                if(a%(255 | (255 << 8)) == b % (255 | (255 << 8)))
+                    counter++;
+            }
+
+            return counter.ToString();
+        }
+
+        public string RunPart2(IEnumerable<string> testData)
+        {
+            var parts = testData.First().Split(',');
+            var a = long.Parse(parts[0]);
+            var b = long.Parse(parts[1]);
+
+            var aFactor = 16807L;
+            var bFactor = 48271L;
+            var divider = 2147483647L;
+            var counter = 0;
+
+            for (long i = 0; i < 40000000; i++)
+            {
+                do
+                {
+                    a = (a * aFactor) % divider;
+                } while (a % 4 != 0);
+
+                do
+                {
+                    b = (b * bFactor) % divider;
+                } while (b % 8 != 0);
+
+                if (a % (255 | (255 << 8)) == b % (255 | (255 << 8)))
+                    counter++;
+            }
+
+            return counter.ToString();
+        }
+    }
+}
---
+++ Chownus.AoC.Tests/2017/15/Day15Tests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Chownus.AoC.Console;
+using Chownus.AoC.Console._2017._15;
+using Xunit;
+
+namespace Chownus.AoC.Tests._2017._15
+{
+    public class Day15Tests
+    {
+        private readonly IAoCSolution _testObject;
+        private readonly IList<string> _input;
+
+        public Day15Tests()
+        {
+            _testObject = new Solution();
+            _input = new List<string>
+            {
+                "65,8921"
+            };
+
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            var solution = _testObject.RunPart1(_input);
+
+            Assert.Equal("588", solution);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            var solution = _testObject.RunPart2(_input);
+
+            Assert.Equal("309", solution);
+        }
+    }
+}
