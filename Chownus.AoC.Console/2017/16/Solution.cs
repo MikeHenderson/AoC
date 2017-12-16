7c2c5048b7aab227d484a99adbf1a82316d17958
Mike Henderson <henderson.mike.j@gmail.com>
2017-12-16 02:24:25 -0600

    Day 16 Solution

---
+++ Chownus.AoC.Console/2017/16/Solution.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace Chownus.AoC.Console._2017._16
+{
+    public class Solution : IAoCSolution
+    {
+        public int Day => 16;
+
+        private static Func<string, MatchCollection> getDigits = s => Regex.Matches(s, "\\d+");
+        private readonly IList<char> alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        private readonly IDictionary<char, Func<string, string, string>> _actions = new Dictionary<char, Func<string, string, string>>
+        {
+            {'s', (c, p) =>
+                {
+                    var numbers = getDigits(c);
+                    return Shift(int.Parse(numbers[0].Value), p);
+                }
+            },
+            {'x', (c, p) =>
+                {
+                    var numbers = getDigits(c);
+                    return Exchange(int.Parse(numbers[0].Value), int.Parse(numbers[1].Value), p);
+                }
+            },
+            {'p', (c, p) => Partner(c[1], c[3], p) }
+        };
+
+        public string RunPart1(IEnumerable<string> testData)
+        {
+            var n = int.Parse(testData.First());
+            var moves = testData.ToList()[1].Split(',');
+            var programs = string.Join(string.Empty, alphabet.Take(n));
+
+            return moves.Aggregate(programs, (current, move) => _actions[move[0]](move, current));
+        }
+
+        public string RunPart2(IEnumerable<string> testData)
+        {
+            var n = int.Parse(testData.First());
+            var moves = testData.ToList()[1].Split(',');
+            var programs = string.Join(string.Empty, alphabet.Take(n));
+            var states = new Dictionary<string, string>();
+            var nDances = 1000000000;
+
+            for (int i = 0; i < nDances; i++)
+            {
+                // Begins repeat at this point
+                if (states.ContainsKey(programs))
+                    break;
+
+                var original = programs;
+                foreach (var move in moves)
+                {
+                    programs = _actions[move[0]](move, programs);
+                }
+
+                states.Add(original, programs);
+            }
+
+            // Find state which would have been the billionth state
+            var index = nDances % states.Count;
+
+            // Get the "billionth" result
+            return states.ElementAt(index).Key;
+        }
+
+        private static string Exchange(int a, int b, string programs)
+        {
+            var arr = programs.ToCharArray();
+            var temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+
+            return string.Join(string.Empty, arr);
+        }
+
+        private static string Shift(int n, string programs)
+        {
+            if (n == 0) return programs;
+            return programs.Substring(programs.Length - n) + programs.Substring(0, programs.Length - n);
+        }
+
+        private static string Partner(char a, char b, string programs)
+        {
+            var aIndex = programs.IndexOf(a);
+            var bIndex = programs.IndexOf(b);
+
+            return Exchange(aIndex, bIndex, programs);
+        }
+    }
+}
---
+++ Chownus.AoC.Tests/2017/16/Day16Tests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Chownus.AoC.Console;
+using Chownus.AoC.Console._2017._16;
+using Xunit;
+
+namespace Chownus.AoC.Tests._2017._16
+{
+    public class Day16Tests
+    {
+        private readonly IAoCSolution _testObject;
+        private readonly IList<string> _input;
+
+        public Day16Tests()
+        {
+            _testObject = new Solution();
+            _input = new List<string>
+            {
+                "5",
+                "s1,x3/4,pe/b"
+            };
+
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            var solution = _testObject.RunPart1(_input);
+
+            Assert.Equal("baedc", solution);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            var solution = _testObject.RunPart2(_input);
+
+            Assert.Equal("abcde", solution);
+        }
+    }
+}
