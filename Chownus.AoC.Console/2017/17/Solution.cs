af82d7764e8379d044b5fdb2fa46c3f48d497c7c
Mike Henderson <henderson.mike.j@gmail.com>
2017-12-16 23:45:38 -0600

    Day 17 Part 1

---
+++ Chownus.AoC.Console/2017/17/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Chownus.AoC.Console.Common.Helpers;
+
+namespace Chownus.AoC.Console._2017._17
+{
+    public class Solution : IAoCSolution
+    {
+        public int Day => 17;
+        public string RunPart1(IEnumerable<string> testData)
+        {
+            var skip = int.Parse(testData.First());
+            var list = new List<int> { 0 };
+            var current = 0;
+
+            for (int i = 1; i < 2018; i++)
+            {
+                current = (current + skip) % list.Count;
+                list.Insert(current + 1, i);
+                current = (current + 1) % list.Count;
+            }
+
+            return list[current + 1].ToString();
+        }
+
+        public string RunPart2(IEnumerable<string> testData)
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ Chownus.AoC.Tests/2017/17/Day17Tests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Chownus.AoC.Console;
+using Chownus.AoC.Console._2017._17;
+using Xunit;
+
+namespace Chownus.AoC.Tests._2017._17
+{
+    public class Day17Tests
+    {
+        private readonly IAoCSolution _testObject;
+        private readonly IList<string> _input;
+
+        public Day17Tests()
+        {
+            _testObject = new Solution();
+            _input = new List<string>
+            {
+                "3"
+            };
+
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            var solution = _testObject.RunPart1(_input);
+            Assert.Equal("638", solution);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+
+        }
+    }
+}
