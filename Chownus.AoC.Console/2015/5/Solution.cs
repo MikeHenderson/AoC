6c7627b0ef5bab297125e3426471e5dc280d786a
Mike Henderson <MHenderson@rightpoint.com>
2017-12-18 15:36:07 -0600

    2015 Day 5 Solution Part 2... hooray regex... not!

---
+++ Chownus.AoC.Console/2015/5/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Chownus.AoC.Console._2015._5
+{
+    public class Solution : IAoCSolution
+    {
+        public int Day => 5;
+        public int Year => 2015;
+
+        public string RunPart1(IEnumerable<string> testData)
+        {
+            var checkVowels = new Func<string, bool>(x => Regex.IsMatch(x, "[aeiou].*[aeiou].*[aeiou]"));
+            var checkLetters = new Func<string, bool>(x => Regex.IsMatch(x, @"(\w)\1+"));
+            var checkClusters = new Func<string, bool>(x => !Regex.IsMatch(x, "ab|cd|pq|xy"));
+
+            return testData.Count(x => checkClusters(x) && checkVowels(x) && checkLetters(x)).ToString();
+        }
+
+        public string RunPart2(IEnumerable<string> testData)
+        {
+            var checkLetters = new Func<string, bool>(x => Regex.IsMatch(x, @"(\w{2}).*\1+"));
+            var checkInBetween = new Func<string, bool>(x => Regex.IsMatch(x, @"(\w).\1"));
+
+            return testData.Count(x => checkInBetween(x) && checkLetters(x)).ToString();
+        }
+    }
+}
