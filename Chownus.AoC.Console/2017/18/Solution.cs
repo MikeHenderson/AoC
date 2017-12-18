895a20a9afda459afbc0b0e3feea13fbb1063d45
Mike Henderson <henderson.mike.j@gmail.com>
2017-12-18 01:48:06 -0600

    Solution Day 18 - note working

---
+++ Chownus.AoC.Console/2017/18/Solution.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace Chownus.AoC.Console._2017._18
+{
+    public class Solution : IAoCSolution
+    {
+        public int Day => 18;
+        public int Year => 2017;
+
+        public string RunPart1(IEnumerable<string> testData)
+        {
+            var commands = testData.ToList();
+            var registers = new Dictionary<char, int>();
+            int lastSoundPlayed = 0;
+            bool received = false;
+            int i = 0;
+
+            while(i < commands.Count && !received)
+            {
+                var parts = commands[i].Split(' ');
+                var x = parts[1][0];
+
+                if (!registers.ContainsKey(x))
+                    registers.Add(x, 0);
+
+                switch (parts[0])
+                {
+                    case "snd":
+                        lastSoundPlayed = registers[x];
+                        break;
+
+                    case "set":
+                        registers[x] = GetValue(parts[2], registers);
+                        break;
+
+                    case "add":
+                        registers[x] += GetValue(parts[2], registers);
+                        break;
+
+                    case "mul":
+                        registers[x] *= GetValue(parts[2], registers);
+                        break;
+
+                    case "mod":
+                        registers[x] %= GetValue(parts[2], registers);
+                        break;
+
+                    case "rcv":
+                        if(registers[x] != 0)
+                            received = true;
+                        break;
+
+                    case "jgz":
+                        if (registers[x] > 0)
+                        {
+                            i += GetValue(parts[2], registers);
+                            continue;
+                        }
+                        break;
+                }
+
+                i++;
+            }
+
+            return lastSoundPlayed.ToString();
+        }
+
+        private int GetValue(string s, IDictionary<char, int> registers)
+        {
+            var isDigit = int.TryParse(s, out var temp);
+            if (isDigit) return temp;
+
+            return registers.ContainsKey(s[0]) ? registers[s[0]] : 0;
+        }
+
+        
+        public string RunPart2(IEnumerable<string> testData)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
